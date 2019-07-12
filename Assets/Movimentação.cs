82f20e3eb60c32342ba7a34b7a57835f0d28f9f5
YagoDelfino <daventhidas@gmail.com>
2019-07-11 21:43:10 -0300

    Jogo sem graça
    adicionando o que fiz testando o unity

---
+++ Assets/Movimentação.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentação : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float fowardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, fowardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+}
---
+++ Assets/colisão.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colisão : MonoBehaviour
+{
+
+    public Movimentação mover;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag == "Obstáculo")
+        {
+            mover.enabled = false;
+        }
+    }
+}
